aca3cc30c9041adc060b3d9d97ccd7657c57077a
ThaiTin-P1 <thaitina5@gmail.com>
2022-02-24 15:25:41 +0700

    add todoReducer and toaster notificate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import AppContent from './component/AppContent';
-import AppHeader from './component/AppHeader';
-import PageTitle from './component/PageTitle';
-import styles from './styles/modules/app.module.scss';
+import React from "react";
+import AppContent from "./component/AppContent";
+import AppHeader from "./component/AppHeader";
+import PageTitle from "./component/PageTitle";
+import styles from "./styles/modules/app.module.scss";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="container">
-      <PageTitle>TODO LIST</PageTitle>
-      <div className={styles.app__wrapper}>
-        <AppHeader />
-        <AppContent />
+    <>
+      <div className="container">
+        <PageTitle>TODO LIST</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
       </div>
-    </div>
+      <Toaster position='bottom-right' toastOption={{
+        style: {
+          fontSize: '1.4rem'
+        }
+      }} />
+    </>
   );
 }
 
--- src/component/TodoModal.js
+++ src/component/TodoModal.js
@@ -1,55 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/modules/modal.module.scss";
 import { MdOutlineClose } from "react-icons/md";
 import Button from "./Button";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../reducers/todoReducer";
+import {v4 as uuid} from 'uuid';
+import toast from "react-hot-toast";
 
 function TodoModal({ modalOpen, setModalOpen }) {
+    const [title, setTitle] = useState('');
+    const [status, setStatus] = useState('incomplete');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(title && status){
+            dispatch(addTodo({
+                id: uuid(),
+                title,
+                status,
+                time: new Date().toLocaleDateString()
+            }))
+            toast.success('Task Added Successfully');
+            setModalOpen(false);
+        }else{
+            toast.error('Title shoudnt be empty')
+        }
+    }
   return (
-    <div>
-      {modalOpen && (
-        <div className={styles.wrapper}>
-          <div className={styles.container}>
-            <div 
-              className={styles.closeButton} 
-              onClick={()=>setModalOpen(false)}
-              onKeyDown={()=>setModalOpen(false)}
-              tabIndex={0}
-              role='button'>
-              <MdOutlineClose />
-            </div>
-            <form className={styles.form}>
-              <h1 className={styles.formTitle}>Add Task</h1>
-
-              <label htmlFor="title">
-                Title
-                <input type="text" id="title" />
-              </label>
-              <label htmlFor="status">
-                Status
-                <select name="status" id="status">
-                  <option value="incomplete">Incomplete</option>
-                  <option value="complete">Complete</option>
-                </select>
-              </label>
-
-              <div className={styles.buttonContainer}>
-                <Button type="submit" variant="primary">
-                  Add Task
-                </Button>
-                <Button 
-                  type="button" 
-                  variant="secondary" 
-                  onClick={()=>setModalOpen(false)}
-                  onKeyDown={()=>setModalOpen(false)}>
-                  Cancel
-                </Button>
-              </div>
-            </form>
+    modalOpen && (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div
+            className={styles.closeButton}
+            onClick={() => setModalOpen(false)}
+            onKeyDown={() => setModalOpen(false)}
+            tabIndex={0}
+            role="button"
+          >
+            <MdOutlineClose />
           </div>
+
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <h1 className={styles.formTitle}>Add Task</h1>
+            <label htmlFor="title">
+              Title
+              <input type="text" id="title" value={title}
+              onChange={(e)=> setTitle(e.target.value)}/>
+            </label>
+            <label htmlFor="status">
+              Status
+              <select name="status" id="status" value={status}
+              onChange={(e)=> setStatus(e.target.value)}>
+                <option value="incomplete">Incomplete</option>
+                <option value="complete">Complete</option>
+              </select>
+            </label>
+
+            <div className={styles.buttonContainer}>
+              <Button type="submit" variant="primary">
+                Add Task
+              </Button>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setModalOpen(false)}
+                onKeyDown={() => setModalOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
         </div>
-      )
-      }
-    </div>
+      </div>
+    )
   );
 }
 
